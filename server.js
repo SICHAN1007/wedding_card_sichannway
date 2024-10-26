87945451e330a5a8c42b48214bca985ad898c1dc
Glitch (glitch-hello-node) <none>
2024-10-26 08:03:38 +0000

    🕴✌️ Checkpoint ./server.js:71335628/279

--- server.js
+++ server.js
@@ -236,31 +236,30 @@ app.post("/api/data", async (req, res) => {
 // 데이터 삭제 API 엔드포인트 추가 
 app.delete("/api/data/", async (req, res) => {
   const { id , pw ,name, title, icon ,date ,Num} = req.body;
-  if(Number(Num)==0){
+  if(Num==="0"){
     try {
       await deleteFromDatabase(id, pw);
-      const data = await getDatabase();
-      res.status(204).json(data); // 삭제 성공 시 204 No Content 응답
+      res.status(204).send; // 삭제 성공 시 204 No Content 응답
     } catch (error) {
       res.status(500).send(error.message);
     }
   }else{
     const storedPw = await getPagePw(id);
   
-  if (storedPw === pw) {
-  // 업데이트할 속성 설정
+    if (storedPw === pw) {
+    // 업데이트할 속성 설정
 
-    try {
-      const updatedData = await updateDatabase(id, name, title, icon, date);
-      const data = await getDatabase();
-      res.status(200).json(data);
-    } catch (error) {
-      res.status(500).send(error.message);
+      try {
+        const updatedData = await updateDatabase(id, name, title, icon, date);
+        const data = await getDatabase();
+        res.status(200).json(data);
+      } catch (error) {
+        res.status(500).send(error.message);
+      }
     }
-  }
-    else {
-    throw new Error("Provided password does not match.");
-  }
+      else {
+      throw new Error("Provided password does not match.");
+    }
   }
 });
 
